819364d34c2145805cd0ceaf919b71800f124b3a
ragnar <ragnar@rmgk.de>
2018-09-04 23:22:42 +0200

    experiment with parsing asciidoc

---
+++ build.sbt
@@ -0,0 +1,33 @@
+name := "asciidocii"
+scalaVersion := "2.12.6"
+organization := "de.rmgk"
+
+enablePlugins(JavaAppPackaging)
+
+libraryDependencies ++= Seq(
+  "org.asciidoctor" % "asciidoctorj" % "1.6.0-alpha.7",
+  "com.lihaoyi" %% "scalatags" % "0.6.7",
+  "com.monovore" %% "decline" % "0.5.0",
+  "com.github.pathikrit" %% "better-files" % "3.6.0",
+  "com.lihaoyi" %% "fastparse" % "1.0.0",
+)
+
+Compile / compile / scalacOptions ++= Seq(
+  "-deprecation",
+  "-encoding", "UTF-8",
+  "-unchecked",
+  "-feature",
+  "-Xlint",
+  "-Xfuture",
+  //"-Xlog-implicits" ,
+  //"-Yno-predef" ,
+  //"-Yno-imports" ,
+  "-Xfatal-warnings",
+  //"-Yinline-warnings" ,
+  "-Yno-adapted-args",
+  //"-Ywarn-dead-code" ,
+  "-Ywarn-nullary-override",
+  "-Ywarn-nullary-unit",
+  "-Ywarn-numeric-widen",
+  //"-Ywarn-value-discard" ,
+)
---
+++ src/main/scala/asciidocii/AsciidociiParser.scala
@@ -0,0 +1,30 @@
+package asciidocii
+
+import fastparse.all._
+
+case class Header(title: String, attributes: Map[String, String])
+sealed trait Block
+case class BlockMacro(command: String, target: String, attributes: String) extends Block
+case class Paragraph(text: String) extends Block
+case class Document(header: Header, blocks: Seq[Block])
+
+object AsciidociiParser {
+  val nl                             = P("\n")
+  val ws                             = P(CharsWhile(_.isWhitespace).opaque("<whitespace>"))
+  val notNL                          = P(CharPred(_ != '\n')).opaque("<not nl>")
+  val notNLS    : Parser[Unit]       = P(notNL | (notNL ~ notNLS))
+  val line                           = P(CharsWhile(_ != '\n')).opaque("<line>")
+  val title                          = P("=" ~/ line.! ~ nl)
+  val word                           = P(CharsWhile(_.isLetterOrDigit)).opaque("<word>")
+  val attribute                      = P(":" ~/ word.! ~ ":" ~/ line.! ~ nl)
+  val header    : Parser[Header]     = P(title ~/ attribute.rep).map { case (title, attr) => Header(title, attr.toMap) }
+  val macroAttributes                = P("[" ~ CharsWhile(c => c != '\n' && c != ']').! ~ "]")
+  val macroTarget                    = P(CharsWhile(c => c != '\n' && c != '['))
+  val blockMacro: Parser[BlockMacro] = P(word.! ~ "::" ~/ macroTarget.! ~ macroAttributes)
+                                       .map {(BlockMacro.apply _).tupled}
+  val paragraph : Parser[Paragraph]  = P(line.rep(min = 1, sep = nl ~ Pass).!).map(Paragraph.apply)
+  val block     : Parser[Block]      = P(blockMacro | paragraph)
+  val document                       = P(header ~ ws ~ block.rep(sep = nl ~ ws.?) ~ ws ~ End)
+                                       .map((Document.apply _).tupled)
+
+}
