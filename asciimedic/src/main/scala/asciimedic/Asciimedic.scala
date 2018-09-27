d2a6b48d2009b9489fc9aa33cf4e187fa6e1c09f
ragnar <ragnar@rmgk.de>
2018-09-27 21:50:55 +0200

    block roles

--- asciimedic/src/main/scala/asciimedic/Asciimedic.scala
+++ asciimedic/src/main/scala/asciimedic/Asciimedic.scala
@@ -1,6 +1,5 @@
 package asciimedic
 
-import fastparse.all
 import fastparse.all._
 
 case class Document(header: Option[Header], blocks: Seq[Block])
@@ -17,7 +16,21 @@ object BlockType {
 
 
 sealed trait Block
-case class BlockWithAttributes(block: Block, attributes: Seq[Seq[Attribute]], title: Option[String]) extends Block
+
+case class BlockWithAttributes(block        : Block,
+                               rawAttributes: Seq[Seq[Attribute]],
+                               title        : Option[String]
+                              ) extends Block {
+  lazy val attributes: Seq[Attribute]      = rawAttributes.flatten
+  lazy val positional: Seq[String]         = attributes.collect { case Attribute("", value) => value }
+  lazy val named     : Map[String, String] =
+    attributes.collect { case Attribute(id, value) if id.nonEmpty => (id, value) }.toMap
+  lazy val role: Seq[String] = {
+    val namedRoles = named.get("role").fold(Seq.empty[String])(_.split(',').toSeq)
+    val positionalRoles = positional.filter(_.startsWith(".")).flatMap(_.split('.').toSeq)
+    (namedRoles ++ positionalRoles).map(_.trim).filter(_.nonEmpty)
+  }
+}
 case class NormalBlock(blockType: BlockType, content: String) extends Block
 case class BlockMacro(command: String, target: String, attributes: Seq[Attribute]) extends Block
 case class SectionTitle(level: Int, title: String) extends Block
--- vitzen/src/main/scala/vitzen/docparser/HtmlConverter.scala
+++ vitzen/src/main/scala/vitzen/docparser/HtmlConverter.scala
@@ -2,27 +2,25 @@ package vitzen.docparser
 
 import asciimedic._
 import scalatags.Text.implicits._
-import scalatags.Text.tags.{code, div, p, frag, tag, img}
+import scalatags.Text.tags.{code, div, p, frag, tag, img, pre}
 import scalatags.Text.attrs.{href, src, cls}
 
 
 object HtmlConverter {
   def convert(document: Document): String = frag(
-    document.blocks.map(blockToHtml): _*
+    document.blocks.map(blockToHtml(_)): _*
   ).render
 
-  def blockToHtml(b: Block): Frag = b match {
-    case NormalBlock(blockType, text) => p(
-      text
-    )
+  def blockToHtml(b: Block, addModifier: Seq[Modifier] = Nil): Frag = b match {
+
+
 
     case SectionTitle(level, title) => tag("h" + (level + 1))(title)
 
-    case BlockWithAttributes(block, attributes, title) =>
+    case bwa@BlockWithAttributes(block, attributes, title) =>
       frag(
         title.getOrElse("").toString,
-        attributes.toString(),
-        blockToHtml(block)
+        blockToHtml(block, bwa.role.map(c => cls := s" $c "))
       )
 
     case NormalBlock(BlockType.Whitespace, _) => frag()
@@ -31,6 +29,14 @@ object HtmlConverter {
       div(cls := "imageblock",
           img(src := target)
       )
+
+    case NormalBlock(blockType, text) => {
+      blockType match {
+        case BlockType.Delimited(delimiter) if delimiter.startsWith(".") => p(text, cls:=" literalblock ")(addModifier : _*)
+        case other =>       p(text)
+
+      }
+    }
 
     case other => div(stringFrag(other.toString))
   }
