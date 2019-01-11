8de4d582148753e94afeea0644536c51db1fcd92
ragnar <ragnar@rmgk.de>
2019-01-11 20:39:05 +0100

    fix paragraph space eating

--- src/main/scala/scitzen/parser/ParagraphParsers.scala
+++ src/main/scala/scitzen/parser/ParagraphParsers.scala
@@ -16,62 +16,62 @@ object ParagraphParsers {
   // plan: only keep macro form, less magic syntax
   // https://asciidoctor.org/docs/user-manual/#pass-macros
 
-  def quoteChars[_:P]: P[Unit] = CharIn("_*`^~#")
+  def quoteChars[_: P]: P[Unit] = CharIn("_*`^~#")
 
   // `<` cross reference; `/` comment; `\` escape; ` `
-  def otherSpecialChars [_:P]: P[Unit] = CharIn("</\\\\")
+  def otherSpecialChars[_: P]: P[Unit] = CharIn("</\\\\")
 
-  def specialCharacter [_:P]: P[Unit] = P(quoteChars | otherSpecialChars)
+  def specialCharacter[_: P]: P[Unit] = P(quoteChars | otherSpecialChars)
 
-  def escaped          [_:P]: P[InlineText] = P("\\" ~ (MacroParsers.start | Attributes.reference | specialCharacter).!)
-                                              .map(InlineText)
+  def escaped[_: P]: P[InlineText] = P("\\" ~ (MacroParsers.start | Attributes.reference | specialCharacter).!)
+                                     .map(InlineText)
 
   /** Simple text stops at special characters or spaces.
-    * Spaces are included, and then we reorient*/
-  def simpleText [_:P]: P[InlineText] = P(untilE(specialCharacter | anySpace)).map(InlineText)
+    * Spaces are included, and then we reorient */
+  def simpleText[_: P]: P[InlineText] = P(untilE(specialCharacter | anySpace)).map(InlineText)
 
-  def special[_:P]: P[Inline] = P(escaped |
-                                  comment |
-                                  MacroParsers.urls.url |
-                                  MacroParsers.inline |
-                                  Attributes.reference |
-                                  crossreference )
+  def special[_: P]: P[Inline] = P(escaped |
+                                   comment |
+                                   MacroParsers.urls.url |
+                                   MacroParsers.inline |
+                                   Attributes.reference |
+                                   crossreference)
 
-  def comment [_:P]: P[InlineMacro] = P(("//" ~ untilI(eol)).rep(1).!)
+  def comment[_: P]: P[InlineMacro] = P(("//" ~ untilI(eol)).rep(1).!)
                                       .map(InlineMacro("//", _, Nil))
 
-  def crossreference [_:P]: P[InlineMacro] = P(quoted(open = "<<", close = ">>"))
+  def crossreference[_: P]: P[InlineMacro] = P(quoted(open = "<<", close = ">>"))
                                              .map(InlineMacro("<<", _, Nil))
 
-  def fullParagraph [_:P]: P[Seq[Inline]] = P(InnerParser().inlineSequence ~ End)
+  def fullParagraph[_: P]: P[Seq[Inline]] = P(inlineSequence ~ anySpaces ~ End)
 
-
-
-case class InnerParser[Res](wantToClose: String = "", constrained: Boolean = false) {
-
-
-    def quotes[_:P](rep: Int): P[InlineQuote] = P {
-        quoteChars.rep(min = rep, max = rep).!.flatMap { delimiter =>
-          InnerParser(delimiter, delimiter.length == 1).inlineSequence.map(v => (delimiter, v))
-        }
-    }.map { case (q, inner) => InlineQuote(q, inner) }
-
-    def closing[_:P]: P[Unit] = P{
-      if (!constrained) wantToClose ~ wantToClose
-      else wantToClose ~ !CharPred(c => c.isLetterOrDigit || c == '_')
-    }
-
-
-
-    def inlineSequence[_:P]: P[Seq[Inline]] = P {
-      if (constrained) {
-        ((significantAnySpaces ~ quotes(1)) | anySpaces ~ (special | quotes(2) | simpleText | specialCharacter.!.map(InlineText))).rep(0) ~ closing
-      }
-      else {
-        ((significantAnySpaces ~ quotes(1)) | anySpaces ~ (special | quotes(2) | simpleText | specialCharacter.!.map(InlineText))).rep(0) ~ anySpaces ~ closing
-      }
-    }
-
+  def inlineSequence[_: P]: P[Seq[Inline]] = P {
+    ((significantAnySpaces.!.map(InlineText) ~ constrainedQuote)
+     | anySpaces.!.map(InlineText) ~ (special | unconstrainedQuote | simpleText | specialCharacter.!.map(InlineText))
+    ).rep(0).map(_.flatMap{case (spaces, inline) => Seq(spaces, inline) })
   }
+
+  def constrainedQuote[_: P]: P[InlineQuote] = P {
+    quoteChars.!.flatMap { delimiter =>
+      (inlineSequence ~ constrainedClosing(delimiter)).map(v => (delimiter, v))
+    }
+  }.map { case (q, inner) => InlineQuote(q, inner) }
+
+  def constrainedClosing[_: P](wantsToClose: String): P[Unit] = P {
+    anySpaces ~ wantsToClose ~ !CharPred(c => c.isLetterOrDigit || c == '_')
+  }
+
+  def unconstrainedQuote[_: P]: P[InlineQuote] = P {
+    quoteChars.!.flatMap { delimiterPart =>
+      val delimiter = delimiterPart + delimiterPart
+      delimiterPart ~ (inlineSequence ~ unconstrainedClosing(delimiter)).map(v => (delimiter, v))
+    }
+  }.map { case (q, inner) => InlineQuote(q, inner) }
+
+  def unconstrainedClosing[_: P](wantsToClose: String): P[Unit] = P {
+    wantsToClose
+  }
+
+
 
 }
