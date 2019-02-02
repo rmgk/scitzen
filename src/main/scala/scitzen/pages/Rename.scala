b382e1c11556dae939d1b51072a94d608af577a9
ragnar <ragnar@rmgk.de>
2019-02-02 16:23:30 +0100

    some refactor on rename

--- src/main/scala/scitzen/pages/Rename.scala
+++ src/main/scala/scitzen/pages/Rename.scala
@@ -11,40 +11,52 @@ import scitzen.converter.DateParsingHelper
 import scitzen.parser.{DocumentParsers, Header}
 
 object Tool {
-  def sluggify(str: String): String = str
-                                      .trim
-                                      .replace("'", "")
-                                      .replaceAll("""[^\p{L}\d]""", "-")
-                                      .replaceAll("-+", "-")
-                                      .replaceAll("^-|-$", "")
+  def sluggify(str: String): String =
+    str
+    .trim
+    .replace("'", "")
+    .replaceAll("""[^\p{L}\d]""", "-")
+    .replaceAll("-+", "-")
+    .replaceAll("^-|-$", "")
 }
 
 object Rename {
+
+  implicit val saneCharsetDefault: Charset = StandardCharsets.UTF_8
+
   val command: Command[Unit] = Command(
     name = "rename",
-    header = "Auto generate file names for posts based on their titles and dates.") {
-    implicit val saneCharsetDefault: Charset = StandardCharsets.UTF_8
-    val optSource = Opts.option[Path]("source", short = "s", metavar = "directory",
-                                      help = "Directory containing Asciidoc source posts")
+    header = "Auto generate file names for posts based on their titles and dates."
+  ) {
+    val optSource = Opts.arguments[Path](metavar = "paths")
 
-
-    optSource.map { sourceP =>
-      val source = File(sourceP)
-      source.children.filter(_.isRegularFile).filter(_.name.endsWith(".adoc")).foreach { f =>
-        val header: Header = fastparse.parse(f.contentAsString, DocumentParsers.header(_)).get.value
-        val date = LocalDateTime.from(DateParsingHelper.relaxedISODateTimeParser.parse(
-          header.attributes.map(a => a.id -> a.value).toMap
-          .getOrElse("revdate", throw new NoSuchElementException(s"${header.title} has no revdate")).trim))
-        val title = Tool.sluggify(header.title) + ".adoc"
-        val newName = date.format(DateParsingHelper.dateOnlyOutput) + "_" + title
-
-        if (newName != f.name) {
-          println(f.name)
-          println(newName)
-          f.renameTo(newName)
-        }
-
-      }
+    optSource.map { sourcefiles =>
+      sourcefiles.map(File(_))
+      .filter(f => f.isRegularFile && f.name.endsWith(".adoc"))
+      .foreach(renameFileFromHeader)
     }
+  }
+
+  def renameFileFromHeader(f: File): Unit = {
+    val header: Header = fastparse.parse(f.contentAsString, DocumentParsers.header(_)).get.value
+    val newName: String = nameFromHeader(header)
+
+    if (newName != f.name) {
+      println(s"rename ${f.name} to $newName")
+      f.renameTo(newName)
+    }
+  }
+
+  def nameFromHeader(header: Header): String = {
+    val headerDateString =
+      header.attributes.map(a => a.id -> a.value).toMap
+      .getOrElse("revdate",
+                 throw new NoSuchElementException(s"${header.title} has no revdate")).trim
+    val date = parseDate(headerDateString)
+    val title = Tool.sluggify(header.title) + ".adoc"
+    date.format(DateParsingHelper.dateOnlyOutput) + "_" + title
+  }
+  private def parseDate(headerDateString: String): LocalDateTime = {
+    LocalDateTime.from(DateParsingHelper.relaxedISODateTimeParser.parse(headerDateString))
   }
 }
