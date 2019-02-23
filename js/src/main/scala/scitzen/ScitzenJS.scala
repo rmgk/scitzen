16c2352a7755127a0646b856683641adb65da7e1
ragnar <ragnar@rmgk.de>
2019-02-23 20:01:32 +0100

    this is already kinda cool :D

--- js/src/main/scala/scitzen/ScitzenJS.scala
+++ js/src/main/scala/scitzen/ScitzenJS.scala
@@ -2,7 +2,7 @@ package scitzen
 
 import org.scalajs.dom
 import org.scalajs.dom.Event
-import org.scalajs.dom.html.{Element, TextArea}
+import org.scalajs.dom.html.TextArea
 import scalatags.JsDom
 import scalatags.JsDom.all._
 import scalatags.JsDom.tags2.{article, main => tagmain}
@@ -11,19 +11,38 @@ import scitzen.parser.Adoc
 
 object ScitzenJS {
 
-  def parseAndStuff(container: Element)(ev: Event): Boolean = {
-    val value = ev.currentTarget.asInstanceOf[TextArea].value
+  def parseAndStuff(ev: Event): Boolean = {
+    val target = ev.currentTarget.asInstanceOf[TextArea]
+    val value = target.value
     println(s"current vlue is $value")
     val doc = Adoc.document(value).get
+
+    if (doc.blocks.isEmpty) return false
+
     val res = new HtmlConverter(JsDom).convert(doc)
-    container.innerHTML = ""
-    container.appendChild(res.render)
+    val parent = target.parentElement
+    val rendered = article(cls := "fullpost", res).render
+    def resetHandler(ev: Event) = {
+      parent.replaceChild(target, rendered)
+      target.focus()
+      false
+    }
+    rendered.addEventListener("dblclick", resetHandler)
+    parent.replaceChild(rendered, target)
     false
+  }
+
+  def resizetoContent(ev: Event) = {
+    val target = ev.currentTarget.asInstanceOf[TextArea]
+    target.style.height = ""
+    target.style.height = target.scrollHeight + "px"
   }
 
   def main(args: Array[String]): Unit = {
     println("hello world")
-    val container = article(cls := "fullpost", contenteditable := "true").render
-    dom.document.body = body(textarea(oninput := parseAndStuff(container) _), tagmain(container)).render
+    dom.document.body = body(tagmain(textarea(onblur := parseAndStuff _,
+                                              oninput := resizetoContent _))).render
   }
 }
+
+
