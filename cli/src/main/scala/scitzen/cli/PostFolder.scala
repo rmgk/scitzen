3338067c35fe093a93f1d845947fc280fdc39c81
ragnar <ragnar@rmgk.de>
2019-04-19 19:45:10 +0200

    better error messages

--- cli/src/main/scala/scitzen/cli/PostFolder.scala
+++ cli/src/main/scala/scitzen/cli/PostFolder.scala
@@ -3,13 +3,20 @@ package scitzen.cli
 import java.nio.file.Path
 
 import better.files._
+import fastparse.Parsed.{Failure, Success}
 import scitzen.converter.Post
-import scitzen.parser.DocumentParsers
+import scitzen.parser.{Document, DocumentParsers}
 
 class PostFolder(basedir: Path) {
   def makePost(path: Path): Post = {
     val content = File(path).contentAsString
-    val document = fastparse.parse(content, DocumentParsers.document(_)).get.value
+    val document = fastparse.parse(content, DocumentParsers.document(_)) match {
+      case f: Failure =>
+        scribe.error(f.trace().longMsg)
+        f.get
+      case s: Success[Document] => s.get.value
+    }
+
     new Post(document, targetPath(path), path.toString, content)
   }
 
