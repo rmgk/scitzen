befffbf24bf6ace84ac25f5d79ee34011a1ac811
ragnar <ragnar@rmgk.de>
2019-04-24 23:12:04 +0200

    allow empty macros

--- core/src/main/scala/scitzen/parser/MacroParsers.scala
+++ core/src/main/scala/scitzen/parser/MacroParsers.scala
@@ -6,8 +6,8 @@ import scitzen.parser.CommonParsers._
 
 object MacroParsers {
   // ensure consistency between detect start and start
-  def detectStart[_ : P]: P[Unit] = P(identifier ~ "[")
-  def start[_: P]: P[String] = P(":" ~ identifier.! ~ &("["))
+  def detectStart[_ : P]: P[Unit] = P(identifier.? ~ "[")
+  def start[_: P]: P[String] = P(":" ~ identifier.?.! ~ &("["))
 
   def full[_: P]: P[Macro] = P(start ~ Attributes.list).map { (Macro.apply _).tupled }
 
