bd5c351ee77fbc1498da96ba136f1581d20bd307
ragnar <ragnar@rmgk.de>
2019-04-28 18:40:50 +0200

    enable author parsing

--- cli/src/main/scala/scitzen/cli/TexPages.scala
+++ cli/src/main/scala/scitzen/cli/TexPages.scala
@@ -1,6 +1,7 @@
 package scitzen.cli
 
 import scitzen.converter.SastToTexConverter
+import scitzen.parser.{Macro, Parse}
 import scitzen.semantics.Sast
 import scitzen.semantics.SastAnalyzes.AnalyzeResult
 
@@ -20,12 +21,21 @@ object TexPages {
 
   def wrap(analyzed: AnalyzeResult, sast: Sast, texType: String, bibliography: Option[String]) = {
     val content = new SastToTexConverter(analyzed).sastToTex(sast)
+    val authors = analyzed.named.get("authors").toList.flatMap {aut =>
+      Parse.paragraph(aut).right.get.collect{
+        case Macro("author", attributes) => (attributes.head.value, attributes.last.value)
+      }
+    }
+    val authorstrings = authors.map{ case (name, inst) =>
+      s"""\\author{$name}
+\\affiliation{\\institution{$inst}}"""
+    }
     texType match {
       case "acmconf" =>
         (List(
           acmHeader(),
           s"\\begin{document}"
-          ) ++ content ++
+          ) ++ authorstrings ++ content ++
          List(
            s"\\bibliographystyle{ACM-Reference-Format}",
            bibliography.fold("")(bib => s"\\bibliography{$bib}"),
