a475f634ed3abfd252a09d3c3b1e277d76bb623e
ragnar <ragnar@rmgk.de>
2019-05-15 22:00:17 +0200

    move genindex

--- src/main/scala/scitzen/generic/DocumentManager.scala
+++ src/main/scala/scitzen/generic/DocumentManager.scala
@@ -2,8 +2,6 @@ package scitzen.generic
 
 import better.files.File
 import scitzen.cli.ParsedDocument
-import scitzen.generic.Sast.{MacroBlock, Section, Text}
-import scitzen.parser.{Attribute, Attributes, InlineText, Macro}
 
 object Months {
   val en = Array("January", "February", "March", "April", "May", "June",
@@ -28,39 +26,3 @@ class DocumentManager(_documents: List[ParsedDocument]) {
 
 }
 
-object GenIndexPage {
-
-
-  def makeIndex(dm: DocumentManager, reverse: Boolean = false): List[Sast] = {
-    def ordering[T: Ordering]:Ordering[T] = if (reverse) Ordering[T].reverse else Ordering[T]
-
-    def sectionBy(pdocs: List[ParsedDocument])
-                 (f: ParsedDocument => String)
-                 (cont: List[ParsedDocument] => List[Sast]) = {
-      val years = pdocs.groupBy(f)
-      years.toList.sortBy(_._1)(ordering).map { case (year, docs) =>
-        Section(Text(List(InlineText(year))), cont(docs))
-      }
-    }
-
-    def secmon(d: ParsedDocument) = {
-      d.sdoc.date.fold("(???)"){date =>
-        val m = date.date.month
-        m +" " + Months.en(m.toInt - 1)
-      }
-    }
-
-
-    sectionBy(dm.documents)(_.sdoc.date.fold("(???)")(_.year)) { docs =>
-      sectionBy(docs)(secmon) { idocs =>
-        idocs.sortBy(_.sdoc.date)(ordering).map { doc =>
-          MacroBlock(Macro("include",
-                           Attributes(List(
-                             Attribute("", doc.file.pathAsString),
-                             Attribute("type", "article")))))
-        }
-      }
-    }
-  }
-
-}
