3d5e8786628a73a48ec60684c3b4e9fda4573ed8
ragnar <ragnar@rmgk.de>
2019-11-30 17:02:54 +0100

    ignore hidden

--- src/main/scala/scitzen/generic/DocumentDiscovery.scala
+++ src/main/scala/scitzen/generic/DocumentDiscovery.scala
@@ -3,7 +3,7 @@ package scitzen.generic
 import java.nio.file.Path
 
 import better.files.File
-import cats.data.NonEmptyList
+import cats.data.{Chain, NonEmptyList}
 
 
 case class DocumentDiscovery(sourcePaths: List[File]) {
@@ -11,13 +11,17 @@ case class DocumentDiscovery(sourcePaths: List[File]) {
   val fileEnding  = "scim"
   val globPattern = "**." + fileEnding
 
-  lazy val sourceDirectories: List[File] = sourcePaths.filter(_.isDirectory)
+  def isScim(f: File) = f.extension(includeDot = false, toLowerCase = true).contains(fileEnding)
+
   lazy val sourceFiles      : List[File] = sourcePaths.filter(_.exists).flatMap {
-    case f if f.isRegularFile => List(f)
-    case f if f.isDirectory   =>
-      f.glob(globPattern).toList
-      // extension also checks if the file is a regular file and exists
-      .filter(f => f.extension(includeDot = false, toLowerCase = true).contains(fileEnding))
+    case f if f.isRegularFile             => List(f)
+    case outerDir if outerDir.isDirectory =>
+      def rec(file: File): Chain[File] = file match {
+        case f if isScim(f) && !f.isHidden => Chain(f)
+        case dir if dir.isDirectory && !dir.isHidden => Chain.fromSeq(dir.children.toList).flatMap(rec)
+        case _ => Chain.empty
+      }
+      rec(outerDir).toList
   }
 }
 
