a7d3906df025b103bb8f21543e4c5d7a4e91c0d3
ragnar <ragnar@rmgk.de>
2020-05-17 19:38:17 +0200

    add html toc generation inside of parsed blocks

--- src/main/scala/scitzen/outputs/HtmlToc.scala
+++ src/main/scala/scitzen/outputs/HtmlToc.scala
@@ -2,7 +2,7 @@ package scitzen.outputs
 
 import scalatags.Text.all._
 import scitzen.generic.Sast
-import scitzen.generic.Sast.Section
+import scitzen.generic.Sast.{Parsed, SBlock, Section}
 
 object HtmlToc {
 
@@ -13,10 +13,10 @@ object HtmlToc {
 
   def tableOfContentsS(document: Seq[Sast], tocDepth: Int): Option[Frag] = {
 
-    if (tocDepth != 2) scribe.warn(s"toc with more then one level of depth currently unsuported")
+    //if (tocDepth != 2) scribe.warn(s"toc with more then one level of depth currently unsuported")
 
     def makeToc(cont: Seq[Sast], depth: Int): Option[Tag] = {
-      val sections = cont.collect { case s: Section if (s.level == 2) => s }
+      val sections = getSections(cont)
       if (sections.length <= 1) None
       else {
         Some(ol(sections.map {
@@ -31,5 +31,12 @@ object HtmlToc {
     document match {
       case other => makeToc(other, tocDepth)
     }
+  }
+
+  private def getSections(cont: Seq[Sast]): Seq[Section] = {
+    cont.collect {
+      case s: Section => List(s)
+      case SBlock(_, Parsed(_, content)) => getSections(content)
+    }.flatten
   }
 }
