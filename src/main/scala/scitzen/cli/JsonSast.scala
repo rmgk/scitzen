14e87d241171937f5bd00cd67228af09758c24ce
ragnar <ragnar@rmgk.de>
2020-07-12 20:30:25 +0200

    output sast to sast converted json

--- src/main/scala/scitzen/cli/JsonSast.scala
+++ src/main/scala/scitzen/cli/JsonSast.scala
@@ -1,41 +1,28 @@
 package scitzen.cli
 
 import java.nio.charset.{Charset, StandardCharsets}
-import java.nio.file.Path
 
 import better.files.File
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
-import com.monovore.decline.{Command, Opts}
+import scitzen.generic.Project.ProjectConfig
+import scitzen.generic.{ConversionContext, Project}
+import scitzen.outputs.SastToSastConverter
 import scitzen.parser._
 
 object JsonSast {
 
   implicit val saneCharsetDefault: Charset = StandardCharsets.UTF_8
 
-  implicit val SastEncoder: JsonValueCodec[Seq[Sast]] =
+  implicit val SastEncoder: JsonValueCodec[List[Sast]] =
     JsonCodecMaker.make(CodecMakerConfig.withAllowRecursiveTypes(true))
 
-  val command: Command[Unit] = Command(name = "json", header = "Convert Scim to Json") {
-
-    Opts.arguments[Path](metavar = "paths").map {
-      _.map(File(_))
-        .filter(_.isRegularFile)
-        .foreach { file =>
-          val content = file.contentAsString
-          val sast    = Parse.documentUnwrap(content, Prov(0, content.length))
-          val target  = file.sibling(file.name + ".json")
-          val json    = writeToArray(sast, WriterConfig.withIndentionStep(2))(SastEncoder)
-          target.writeByteArray(json)
-        }
-    }
-
-  }
-
   def jsonFor(file: File): String = {
-    val content = file.contentAsString
-    val sast    = Parse.documentUnwrap(content, Prov(0, content.length))
-    writeToString(sast)(SastEncoder)
+    val content   = file.contentAsString
+    val sast      = Parse.documentUnwrap(content, Prov(0, content.length))
+    val converter = new SastToSastConverter(Project(file.parent, ProjectConfig()), file, _ => "", None)
+    val converted = converter.convertSeq(sast)(ConversionContext(())).data.toList
+    writeToString(converted)(SastEncoder)
   }
 
 }
