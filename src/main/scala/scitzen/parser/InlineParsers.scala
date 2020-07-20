c9ce3281210201dfcfa592430da8408c765a79f9
ragnar <ragnar@rmgk.de>
2020-07-20 18:07:37 +0200

    simplify non-syntax inline parser, fix parsing : at start

--- src/main/scala/scitzen/parser/InlineParsers.scala
+++ src/main/scala/scitzen/parser/InlineParsers.scala
@@ -6,18 +6,15 @@ import fastparse._
 object InlineParsers {
 
   private def notSyntax[_: P]: P[String] =
-    P((CharsWhile(_ != ':') ~ (!MacroParsers.syntaxStart ~ ":").rep(0)).rep(
-      min = 1,
-      sep = !MacroParsers.syntaxStart
-    ).!)
+    P((CharsWhile(_ != ':') | (!MacroParsers.syntaxStart ~ ":")).rep(1).!)
 
   def simpleText[_: P]: P[InlineText] = {
     P(notSyntax.!).map(InlineText)
   }
 
-  def fullParagraph[_: P]: P[Seq[Inline]] =
-    P(inlineSequence ~ End)
-
   def inlineSequence[_: P]: P[Seq[Inline]] =
     P((MacroParsers.comment | MacroParsers.full | simpleText).rep(0))
+
+  def fullParagraph[_: P]: P[Seq[Inline]] =
+    P(inlineSequence ~ End)
 }
