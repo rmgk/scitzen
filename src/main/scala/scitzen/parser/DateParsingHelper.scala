d7a15f169d8bb868aeac3ba959dd6e0a6c9d4484
ragnar <ragnar@rmgk.de>
2020-07-21 22:35:32 +0200

    add iso date methods

--- src/main/scala/scitzen/parser/DateParsingHelper.scala
+++ src/main/scala/scitzen/parser/DateParsingHelper.scala
@@ -8,12 +8,14 @@ case class ScitzenDate(year: String, month: String, day: String) {
 }
 case class ScitzenTime(hour: String, minute: String, second: String) {
   def short = s"$hour:$minute"
+  def iso = s"$hour:$minute:${second}Z"
 }
 case class ScitzenDateTime(date: ScitzenDate, timeO: Option[ScitzenTime]) {
   def timeAppend: String   = timeO.fold("")(st => s" ${st.short}")
   def full: String         = s"${date.full}$timeAppend"
   def monthDayTime: String = s"${date.month}-${date.day}$timeAppend"
   def year: String         = date.year
+  def iso: String = s"${date.full}${timeO.fold("")(_.iso)}"
 }
 object ScitzenDateTime {
   implicit val ord: Ordering[ScitzenDateTime] = Ordering.by(_.full)
