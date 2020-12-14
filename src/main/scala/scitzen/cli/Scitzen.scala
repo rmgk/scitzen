e35f7ac097de42ef678310a81dd2ab274074e603
ragnar <ragnar@rmgk.de>
2020-12-14 13:55:57 +0100

    refactor converter – fix native image init

--- src/main/scala/scitzen/cli/Scitzen.scala
+++ src/main/scala/scitzen/cli/Scitzen.scala
@@ -8,16 +8,12 @@ import scitzen.generic.{DocumentDirectory, Project}
 import scribe.Logger
 
 import java.nio.file.{Path, Paths}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 
 object Scitzen
     extends CommandApp(
       name = "scitzen",
       header = "Static page generator",
       main = {
-
         import scribe.format._
         val myFormatter: Formatter = formatter"$message ($position)"
         Logger.root.clearHandlers().withHandler(
@@ -49,55 +45,70 @@ object ConvertProject {
   val command: Command[Unit] = Command(name = "gen", header = "Convert Scim to Sast.") {
     args.mapN {
       (sourcedirRel, syncFileRelOption, syncPos, imageFileMap, printJson) =>
-        val starttime = System.nanoTime()
-        var lasttime  = starttime
-        def timediff: String = {
-          val now           = System.nanoTime()
-          def diff(t: Long) = (now - t) / 1000000
-          val res           = s"(${diff(starttime)}ms|${diff(lasttime)}ms)"
-          lasttime = now
-          res
-        }
         printJson match {
           case Some(path) => println(JsonSast.jsonFor(File(path)))
           case None =>
             Project.fromSource(File(sourcedirRel)) match {
               case None => scribe.error(s"could not find project for $sourcedirRel")
               case Some(project) =>
-                scribe.info(s"found project in ${project.root} $timediff")
-                val documentDirectory = DocumentDirectory(project.root)
-                scribe.info(s"parsed ${documentDirectory.documents.size} documents $timediff")
-                if (project.config.format.contains("content")) {
-                  Format.formatContents(documentDirectory)
-                  scribe.info(s"formatted contents $timediff")
-                }
-                if (project.config.format.contains("filename")) {
-                  Format.formatRename(documentDirectory)
-                  scribe.info(s"formatted filenames $timediff")
-                }
-                val fHtml = Future {
-                  if (project.config.outputType.contains("html")) {
-                    val sync = syncFileRelOption.map2(syncPos)((f, p) => File(f) -> p)
-                    ConvertHtml.convertToHtml(project, sync, documentDirectory)
-                    scribe.info(s"generated html $timediff")
-                  }
-                }
-                val fPdf = Future {
-                  if (project.config.outputType.contains("pdf")) {
-                    ConvertPdf.convertToPdf(project, documentDirectory)
-                    scribe.info(s"generated pdfs $timediff")
-                  }
-                }
-                val fIfm = Future {
-                  if (imageFileMap) {
-                    ImageReferences.listAll(project, documentDirectory)
-                    scribe.info(s"generated imagemap $timediff")
-                  }
-                }
-                List(fHtml, fPdf, fIfm).foreach(Await.ready(_, Duration.Inf))
-
+                executeConversions(syncFileRelOption, syncPos, imageFileMap, project)
             }
         }
     }
+  }
+
+  def makeTimediff(): () => String = {
+    val starttime = System.nanoTime()
+    var lasttime  = starttime
+    def timediff: String = {
+      val now           = System.nanoTime()
+      def diff(t: Long) = (now - t) / 1000000
+      val res           = s"(${diff(starttime)}ms|${diff(lasttime)}ms)"
+      lasttime = now
+      res
+    }
+    () => timediff
+  }
+
+  private def executeConversions(
+      syncFileRelOption: Option[Path],
+      syncPos: Option[Int],
+      imageFileMap: Boolean,
+      project: Project
+  ): Unit = {
+    val timediff = makeTimediff()
+
+    scribe.info(s"found project in ${project.root} $timediff()")
+    val documentDirectory = DocumentDirectory(project.root)
+    scribe.info(s"parsed ${documentDirectory.documents.size} documents $timediff()")
+    if (project.config.format.contains("content")) {
+      Format.formatContents(documentDirectory)
+      scribe.info(s"formatted contents $timediff()")
+    }
+    if (project.config.format.contains("filename")) {
+      Format.formatRename(documentDirectory)
+      scribe.info(s"formatted filenames $timediff()")
+    }
+    val fHtml = () => {
+      if (project.config.outputType.contains("html")) {
+        val sync = syncFileRelOption.map2(syncPos)((f, p) => File(f) -> p)
+        ConvertHtml.convertToHtml(project, sync, documentDirectory)
+        scribe.info(s"generated html $timediff()")
+      }
+    }
+    val fPdf = () =>  {
+      if (project.config.outputType.contains("pdf")) {
+        ConvertPdf.convertToPdf(project, documentDirectory)
+        scribe.info(s"generated pdfs $timediff()")
+      }
+    }
+    val fIfm = () => {
+      if (imageFileMap) {
+        ImageReferences.listAll(project, documentDirectory)
+        scribe.info(s"generated imagemap $timediff()")
+      }
+    }
+    import scala.jdk.CollectionConverters._
+    List(fHtml, fPdf, fIfm).asJava.parallelStream().forEach(_.apply())
   }
 }
