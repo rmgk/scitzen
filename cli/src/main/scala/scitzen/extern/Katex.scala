ac96ad97f9cedede9bda32bfba0d07acc46b5f84
ragnar <ragnar@rmgk.de>
2021-12-15 17:28:17 +0100

    fix abhorrent performance when initializing katex for each article

--- cli/src/main/scala/scitzen/extern/Katex.scala
+++ cli/src/main/scala/scitzen/extern/Katex.scala
@@ -4,8 +4,8 @@ import java.lang.ProcessBuilder.Redirect
 import org.graalvm.polyglot.*
 import better.files.*
 import org.graalvm.polyglot.proxy.ProxyObject
-import scala.jdk.CollectionConverters._
 
+import scala.jdk.CollectionConverters.*
 import com.github.plokhotnyuk.jsoniter_scala.core.writeToString
 
 object Katex:
@@ -21,23 +21,36 @@ object Katex:
           val res = katex.renderToString(str)
           (res, Some(copy(cache = cache.updated(str, res))))
 
+  end KatexConverter
+
+  // lazily initialize polyglot: pay the cost only once per execution, but
+  // prevent native image from trying to compile the js context
+  lazy val polyglot: Context =
+    val ctx = Context.create()
+    ctx.eval("js", katexstr)
+    ctx
+
   class KatexLibrary(katexdefs: Option[File]):
-    val bindings: Map[String, String] = katexdefs.map { f =>
-      f.lineIterator.map(_.split(":", 2)).collect {
-        case Array(k, v) => (k, v)
-      }.toMap
-    }.getOrElse(Map.empty)
 
-    val jsonBindings = writeToString(bindings)(scitzen.compat.Codecs.mapCodec)
+    // also lazily initialize katex only when render to string is actually called
+    lazy val katex =
+      val bindings: Map[String, String] = katexdefs.map { f =>
+        f.lineIterator.map(_.split(":", 2)).collect {
+          case Array(k, v) => (k, v)
+        }.toMap
+      }.getOrElse(Map.empty)
 
-    val polyglot = Context.create()
-    polyglot.eval("js", katexstr)
-    polyglot.getBindings("js").putMember("macro_bindings", jsonBindings)
-    val katex = polyglot.eval("js", """
-    {const katex_settings = {throwOnError: false, macros: JSON.parse(macro_bindings)};
-    s => katex.renderToString(s, katex_settings) }
-    """)
-    polyglot.getBindings("js").removeMember("macro_bindings")
+      val jsonBindings = writeToString(bindings)(scitzen.compat.Codecs.mapCodec)
+
+
+      polyglot.getBindings("js").putMember("macro_bindings", jsonBindings)
+      val katexRes = polyglot.eval("js", """
+      {const katex_settings = {throwOnError: false, macros: JSON.parse(macro_bindings)};
+      s => katex.renderToString(s, katex_settings) }
+      """)
+      polyglot.getBindings("js").removeMember("macro_bindings")
+      katexRes
 
     def renderToString(string: String): String =
+      scribe.info(s"compiling $string")
       katex.execute(string).asString()
