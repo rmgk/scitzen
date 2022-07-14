327f56bac77ebfc759197db4de8e1bfb52c01ccc
ragnar <ragnar@rmgk.de>
2022-07-14 14:32:10 +0200

    dependency updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,45 +14,46 @@ object Dependencies {
     val circeCore          = "0.14.1"
     val decline            = "2.2.0"
     val directories        = "26"
-    val fastparse          = "2.3.3-35-06d972"
     val jetty              = "9.4.46.v20220331"
     val jol                = "0.16"
-    val jsoniterScalaCore  = "2.13.27"
+    val jsoniterScalaCore  = "2.13.36"
     val jsoniterScalaOld   = "2.13.3" // this is the latest version supporting Scala 2.11 and java 8
-    val jsoup              = "1.15.1"
-    val munit              = "1.0.0-M4"
+    val jsoup              = "1.15.2"
+    val munit              = "1.0.0-M6"
     val normalizecss       = "8.0.1"
-    val okHttp             = "4.9.3"
+    val okHttp             = "4.10.0"
     val pprint             = "0.7.3"
-    val reactiveStreams    = "1.0.3"
+    val quicklens          = "1.8.8"
+    val reactiveStreams    = "1.0.4"
     val retypecheck        = "0.10.0"
     val scala211           = "2.11.12"
     val scala212           = "2.12.15"
     val scala213           = "2.13.8"
-    val scala3             = "3.1.3-RC4"
+    val scala3             = "3.1.3"
     val scalaJavaTime      = "2.3.0"
-    val scalaLoci          = "0.5.0"
+    val scalaLoci          = "609b4c1b58"
     val scalaSwing         = "3.0.0"
     val scalacheck         = "1.16.0"
     val scalactic          = "3.0.0"
-    val scalajsDom         = "2.1.0"
+    val scalajsDom         = "2.2.0"
     val scalatags          = "0.11.1"
     val scalatest          = "3.2.12"
-    val scalatestpluscheck = "3.2.11.0"
-    val scribe             = "3.8.2"
-    val slips              = "3e80b7d8eb"
-    val sourcecode         = "0.2.8"
+    val scalatestpluscheck = "3.2.12.0"
+    val scopt              = "4.1.0"
+    val scribe             = "3.8.3"
+    val slips              = "0.4.4"
+    val sourcecode         = "0.3.0"
     val tomlScala          = "0.2.2"
     val upickle            = "2.0.0"
   }
 
   import Dependencies.{Versions => V}
 
-  val betterFiles     = Def.setting("com.github.pathikrit" %% "better-files" % V.betterFiles)
+  val betterFiles =
+    Def.setting(("com.github.pathikrit" %% "better-files" % V.betterFiles).cross(CrossVersion.for3Use2_13))
   val catsCore        = Def.setting("org.typelevel" %%% "cats-core" % V.catsCore)
   val decline         = Def.setting("com.monovore" %%% "decline" % V.decline)
   val directories     = Def.setting("dev.dirs" % "directories" % V.directories)
-  val fastparse       = Def.setting("com.lihaoyi" %%% "fastparse" % V.fastparse)
   val jetty           = Def.setting("org.eclipse.jetty" % "jetty-rewrite" % V.jetty)
   val jol             = Def.setting("org.openjdk.jol" % "jol-core" % V.jol)
   val jsoup           = Def.setting("org.jsoup" % "jsoup" % V.jsoup)
@@ -61,6 +62,7 @@ object Dependencies {
   val normalizecss    = Def.setting("org.webjars.npm" % "normalize.css" % V.normalizecss)
   val okHttp          = Def.setting("com.squareup.okhttp3" % "okhttp" % V.okHttp)
   val pprint          = Def.setting("com.lihaoyi" %%% "pprint" % V.pprint)
+  val quicklens       = Def.setting("com.softwaremill.quicklens" %%% "quicklens" % V.quicklens)
   val reactiveStreams = Def.setting("org.reactivestreams" % "reactive-streams" % V.reactiveStreams)
   val retypecheck =
     Def.setting(if (`is 3`(scalaVersion.value)) None
@@ -75,7 +77,8 @@ object Dependencies {
   val scalatestpluscheck =
     Def.setting(if (`is 2.11`(scalaVersion.value))
       "org.scalatestplus"    %%% "scalacheck-1-15" % "3.2.4.0-M1"         % "test"
-    else "org.scalatestplus" %%% "scalacheck-1-15" % V.scalatestpluscheck % "test")
+    else "org.scalatestplus" %%% "scalacheck-1-16" % V.scalatestpluscheck % "test")
+  val scopt       = Def.setting("com.github.scopt" %%% "scopt" % V.scopt)
   val scribe      = Def.setting("com.outr" %%% "scribe" % V.scribe)
   val scribeSlf4j = Def.setting("com.outr" %% "scribe-slf4j" % V.scribe)
   val sourcecode  = Def.setting("com.lihaoyi" %%% "sourcecode" % V.sourcecode)
@@ -104,11 +107,12 @@ object Dependencies {
     val chain    = Def.setting("de.rmgk.slips" %%% "chain" % V.slips)
     val category = Def.setting("de.rmgk.slips" %%% "category" % V.slips)
     val scip     = Def.setting("de.rmgk.slips" %%% "scip" % V.slips)
+    val delay    = Def.setting("de.rmgk.slips" %%% "delay" % V.slips)
   }
 
   object loci {
     def generic(n: String): Def.Initialize[sbt.ModuleID] =
-      if (V.scalaLoci.size > 20)
+      if (!V.scalaLoci.contains("."))
         Def.setting("com.github.scala-loci.scala-loci" %%% s"scala-loci-$n" % V.scalaLoci)
       else Def.setting("io.github.scala-loci"          %%% s"scala-loci-$n" % V.scalaLoci)
 
@@ -121,7 +125,7 @@ object Dependencies {
     val wsAkka        = generic("communicator-ws-akka")
     val wsWeb         = generic("communicator-ws-webnative")
     val wsJavalin     = generic("communicator-ws-javalin")
-    val wsJetty       = Def.setting(Seq(generic("communicator-ws-jetty").value, jetty.value))
+    val wsJetty       = generic("communicator-ws-jetty")
   }
 
   // Add JavaFX dependencies, should probably match whatever the scalafx version was tested against:
