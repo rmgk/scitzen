a169c19566f0cdd6be7099896c045738bc573dc4
ragnar <ragnar@rmgk.de>
2022-10-26 12:22:12 +0200

    fix path

--- make-graal-config.fish
+++ make-graal-config.fish
@@ -6,5 +6,5 @@ rm -rf cli/src/main/resources/META-INF
 set -l GH (cs java-home --jvm graalvm-java17:22.3.0)
 $GH/bin/gu install native-image
 $GH/bin/gu install js
-cs launch sbt --jvm graalvm-java17:22.3.0 -- 'set javaOptions += "-agentlib:native-image-agent=config-output-dir=sr/main/resources/META-INF/native-image"' "run Examples --image-file-map imagemap.json"
+cs launch sbt --jvm graalvm-java17:22.3.0 -- 'set javaOptions += "-agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image"' "run Examples --image-file-map imagemap.json"
 rm imagemap.json
