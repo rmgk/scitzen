7f27428fd8e69b8c4b45d5dac14e58c08f3a07d9
ragnar <git@rmgk.de>
2023-06-15 00:27:51 +0200

    undo graal changes

--- contrib/make-graal-config.fish
+++ contrib/make-graal-config.fish
@@ -3,7 +3,7 @@
 rm -rf Examples/scitzen.cache
 rm -rf Examples/scitzen.out
 rm -rf src/main/resources/META-INF/native-image/generated
-# set JAVA_HOME (cs java-home --jvm graalvm-java19:22.3.1)
+set JAVA_HOME (cs java-home --jvm graalvm-java19:22.3.1)
 $JAVA_HOME/bin/gu install native-image
 $JAVA_HOME/bin/gu install js
 cs launch sbt -- 'set fork := true' 'set javaOptions += "-agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image/generated"' "run Examples --image-file-map imagemap.json"
