14b9168cda0da9c0b79f2ced3c9aa9e4f15be664
ragnar <git@rmgk.de>
2023-06-15 00:17:40 +0200

    native image workarounds

--- contrib/make-native-image.sh
+++ contrib/make-native-image.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sbt stageJars
+# we have this dependency due to some JS compat, just remove it here
+rm target/jars/graal-sdk-*.jar
+rm target/jars/truffle-api-*.jar
+rm target/jars/js-*.jar
 native-image --class-path "target/jars/*" scitzen.cli.ScitzenCommandline
