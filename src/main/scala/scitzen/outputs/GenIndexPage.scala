44d83ddd1e09722b67498b2c3ae8634a442d7bf5
ragnar <git@rmgk.de>
2023-06-18 22:07:01 +0200

    improve index page generation

--- src/main/scala/scitzen/outputs/GenIndexPage.scala
+++ src/main/scala/scitzen/outputs/GenIndexPage.scala
@@ -2,7 +2,7 @@ package scitzen.outputs
 
 import scitzen.generic.{Project, TitledArticle}
 import scitzen.sast.DCommand.Other
-import scitzen.sast.{Attribute, Attributes, BCommand, Block, Directive, InlineText, Parsed, Prov, Sast, Section, Text}
+import scitzen.sast.{Attribute, Attributes, DCommand, Directive, InlineText, Prov, Sast, ScitzenDateTime, Section, Text}
 
 import java.nio.file.Path
 import scala.collection.immutable.ArraySeq
@@ -28,47 +28,57 @@ object GenIndexPage:
     "December"
   )
 
+  def sectionFor(name: String) =
+    if name.isBlank
+    then Section(Text(List()), prefix = "#", Attributes(Seq(Attribute("unique ref", "unnamed"))))(Prov())
+    else Section(Text(List(InlineText(name))), prefix = "#", Attributes(Seq(Attribute("unique ref", name))))(Prov())
+
+  def sectionTitle(fd: TitledArticle): String =
+    fd.header.date.fold("") { date =>
+      val m = date.date.month
+      s"${date.year}-$m " + months(m.toInt - 1)
+    }
+
+  def directiveFor(project: Project, indexDir: Path, doc: TitledArticle): Directive =
+    def categories = doc.named.get("tags").iterator.flatMap(_.split(",")).map(_.trim).filter(!_.isBlank).toList
+    Directive(
+      Other("article"),
+      Attributes(
+        List(
+          Some(Attribute("", doc.header.title)),
+          Some(Attribute(
+            "target",
+            indexDir.relativize(project.htmlPaths.articleOutputPath(doc.header)).toString
+          )),
+          doc.date.map(date => Attribute("datetime", date.dayTime))
+        ).flatten ++ categories.map(Attribute("category", _))
+      )
+    )(Prov())
+
   def makeIndex(
       articles: List[TitledArticle],
       project: Project,
       indexDir: Path
   ): List[Sast] =
-    def ordering[T: Ordering]: Ordering[T] = Ordering[T].reverse
+    if articles.isEmpty then return Nil
 
-    def sectionBy(pdocs: List[TitledArticle])(f: TitledArticle => String)(cont: List[TitledArticle] => List[Sast]): List[Sast] =
-      val sectionTitle = pdocs.groupBy(f)
-      sectionTitle.toList.sortBy(_._1)(ordering).flatMap {
-        case (key, docs) =>
-          val inner = cont(docs)
-          List[Sast](
-            Section(Text(List(InlineText(key))), prefix = "#", Attributes(Seq(Attribute("unique ref", key))))(Prov()),
-            Block(BCommand.Empty, Attributes(Nil), Parsed("", inner))(Prov())
+    given Ordering[Option[ScitzenDateTime]] = Ordering.Option(Ordering.apply[ScitzenDateTime].reverse)
+
+    val ordered = articles.sortBy(f => (f.date, f.title))
+
+    def rec(ordered: List[TitledArticle], acc: List[Sast]): List[Sast] =
+      ordered match
+        case Nil => acc
+        case h :: tail =>
+          val current         = sectionTitle(h)
+          val (include, rest) = tail.span(v => sectionTitle(v) == current)
+          val content         = (h :: include).map(art => directiveFor(project, indexDir, art))
+          rec(
+            rest,
+            (if current.isBlank
+             then Directive(DCommand.Other("break"), Attributes.emtpy)(Prov()) :: content
+             else sectionFor(current) :: content)
+              .:::(acc)
           )
-      }
 
-    def secmon(fd: TitledArticle): String =
-      fd.header.date.fold("Dateless") { date =>
-        val m = date.date.month
-        s"${date.year}-$m " + months(m.toInt - 1)
-      }
-
-    sectionBy(articles)(secmon) { idocs =>
-      idocs.sortBy(_.header.date)(ordering).flatMap { doc =>
-        def categories = doc.named.get("tags").iterator.flatMap(_.split(",")).map(_.trim).filter(!_.isBlank).toList
-        List(Directive(
-          Other("article"),
-          Attributes(
-            List(
-              Some(Attribute("", doc.header.title)),
-              Some(Attribute(
-                "target",
-                indexDir.relativize(project.htmlPaths.articleOutputPath(doc.header)).toString
-              )),
-              doc.date.map(date => Attribute("datetime", date.dayTime))
-            ).flatten ++ categories.map(Attribute("category", _))
-          )
-        )(
-          Prov()
-        ))
-      }
-    }
+    rec(ordered, Nil)
