3f757fa0cfc41abbe7b650fbaadedecab9f8d9d2
ragnar <git@rmgk.de>
2023-06-30 10:45:59 +0200

    use those cores!

--- src/main/scala/scitzen/cli/ConvertHtml.scala
+++ src/main/scala/scitzen/cli/ConvertHtml.scala
@@ -15,7 +15,9 @@ import scitzen.sast.{Attribute, Attributes, Prov, Section}
 
 import java.nio.charset.{Charset, StandardCharsets}
 import java.nio.file.{Files, Path}
-import scala.annotation.tailrec
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 import scala.math.Ordering.Implicits.seqOrdering
 import scala.util.Using
 
@@ -44,19 +46,15 @@ class ConvertHtml(anal: ConversionAnalysis):
       val katexLibrary = KatexLibrary(project.config.katexMacros.flatMap(project.resolve(project.root, _)))
       KatexConverter(loadKatex(katexmapfile), katexLibrary)
 
-    // val ec = scala.concurrent.ExecutionContext.global
-
-    @tailrec
     def procRec(
         rem: List[TitledArticle],
         resourcemap: Map[ProjectPath, Path],
         done: Set[ArticleRef],
-    ): Map[ProjectPath, Path] =
-      rem match
-        case Nil => (resourcemap)
-        case article :: rest =>
+    ): Future[Map[ProjectPath, Path]] =
+      val futures = rem.map: titled =>
+        Future:
           val cctx = convertArticle(
-            article,
+            titled,
             cssfile,
             sync,
             nlp,
@@ -65,10 +63,12 @@ class ConvertHtml(anal: ConversionAnalysis):
           val found         = cctx.referenced.toSet -- done
           val foundArticles = found.iterator.flatMap(anal.directory.byRef.get).toList
           procRec(
-            foundArticles ::: rest,
+            foundArticles,
             resourcemap ++ cctx.resourceMap,
             found union done
           )
+        .flatten
+      Future.foldLeft(futures)(Map.empty)(_ ++ _)
 
     val selected = anal.directory.fullArticles.iterator.filter: art =>
       anal.selectionPrefixes.exists: sel =>
@@ -76,7 +76,7 @@ class ConvertHtml(anal: ConversionAnalysis):
     val sellist = selected.toList
     if sellist.isEmpty then
       Logging.cli.warn("selection is empty", anal.selectionPrefixes)
-    val resources = procRec(sellist, Map.empty, sellist.iterator.map(_.article.ref).toSet)
+    val resources = Await.result(procRec(sellist, Map.empty, sellist.iterator.map(_.article.ref).toSet), Duration.Inf)
     project.htmlPaths.copyResources(resources)
     writeKatex(katexmapfile, katexConverter.cache.get())
     ()
