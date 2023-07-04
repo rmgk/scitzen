8adb27dfca7e24f50ae11559f80eb0e782d74cb7
ragnar <git@rmgk.de>
2023-07-04 12:55:23 +0200

    drop valid tag&attribute name checks, we just have explicit lists

--- src/main/scala/scitzen/html/Escaping.scala
+++ src/main/scala/scitzen/html/Escaping.scala
@@ -26,39 +26,6 @@ package scitzen.html
   * potentially useful outside of Scalatags.
   */
 object Escaping {
-
-  private[this] val tagRegex = "^[a-z][:\\w0-9-]*$".r
-
-  /** Uses a regex to check if something is a valid tag name. */
-  def validTag(s: String) = tagRegex.unapplySeq(s).isDefined
-
-  /** Check if 's' is a valid attribute name. */
-  def validAttrName(s: String): Boolean = {
-    // this is equivalent of the regex but without a huge amount of object creation.
-    // original regex - ^[a-zA-Z_:][-a-zA-Z0-9_:.]*$
-    // n.b. I know its ugly, but its fast
-    val len = s.length
-    if (len == 0)
-      return false
-
-    val sc             = s.charAt(0)
-    val startCharValid = (sc >= 'a' && sc <= 'z') || (sc >= 'A' && sc <= 'Z') || sc == ':'
-    if (!startCharValid)
-      return false
-
-    var pos = 1
-    while (pos < len) {
-      val c = s.charAt(pos)
-      val valid = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') ||
-        c == '-' || c == ':' || c == '.' || c == '_'
-      if (!valid)
-        return false
-      pos += 1
-    }
-
-    true
-  }
-
   /** Code to escape text HTML nodes. Based on code from scala.xml */
   def escape(text: String, s: java.io.Writer) = {
     // Implemented per XML spec:
