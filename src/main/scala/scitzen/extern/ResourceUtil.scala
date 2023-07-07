c4742e1ea579780465a7a739c548b98193f55c73
ragnar <git@rmgk.de>
2023-07-07 15:28:20 +0200

    put resources in hopefully unique folder, and include that always

--- src/main/scala/scitzen/extern/ResourceUtil.scala
+++ src/main/scala/scitzen/extern/ResourceUtil.scala
@@ -5,7 +5,7 @@ import scala.util.Using
 
 object ResourceUtil {
   def load(path: String): Array[Byte] =
-    Option(getClass.getClassLoader.getResourceAsStream(path)).map { rs =>
+    Option(getClass.getClassLoader.getResourceAsStream(s"de.rmgk.scitzen/$path")).map { rs =>
       Using.resource(rs) { ars =>
         val bo = new ByteArrayOutputStream(14000) // size estimate from file snapshot
         ars.transferTo(bo)
