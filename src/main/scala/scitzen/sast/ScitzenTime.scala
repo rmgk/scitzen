4bf2a41eadfe4b27f7607108bf766ea3cbd609e6
ragnar <git@rmgk.de>
2023-07-27 00:49:55 +0200

    fix date

--- src/main/scala/scitzen/sast/ScitzenTime.scala
+++ src/main/scala/scitzen/sast/ScitzenTime.scala
@@ -6,7 +6,7 @@ case class ScitzenDate(year: String, month: Option[String], day: Option[String])
 }
 case class ScitzenTime(hour: String, minute: Option[String], second: Option[String]) {
   require(if second.isDefined then minute.isDefined else true)
-  def short = hour + minute.map(m => s":$m")
+  def short = hour + minute.map(m => s":$m").getOrElse("")
   def iso   = Iterator(Some(hour), minute, second).flatten.mkString("", ":", "Z")
 }
 case class ScitzenDateTime(date: ScitzenDate, timeO: Option[ScitzenTime]) {
