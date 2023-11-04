266e233d855c9fc7f3e3274f1ea33caac2cdce33
ragnar <git@rmgk.de>
2023-11-04 23:44:41 +0100

    it’s the callers problem now to figure out graal

--- contrib/make-graal-config.fish
+++ contrib/make-graal-config.fish
@@ -3,8 +3,5 @@
 rm -rf Examples/scitzen.cache
 rm -rf Examples/scitzen.out
 rm -rf src/main/resources/META-INF/native-image/generated
-set JAVA_HOME (cs java-home --jvm graalvm-java21:21.0.1)
-$JAVA_HOME/bin/gu install native-image
-$JAVA_HOME/bin/gu install js
 cs launch sbt -- 'set fork := true' 'set javaOptions += "-agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image/generated"' "run Examples --image-file-map imagemap.json"
 rm imagemap.json
