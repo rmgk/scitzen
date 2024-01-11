1c1dc29d20203682cd0794db8331821b04f04526
ragnar <git@rmgk.de>
2024-01-11 20:47:06 +0100

    fails every time – not sure I care

--- src/main/scala/scitzen/sast/Sast.scala
+++ src/main/scala/scitzen/sast/Sast.scala
@@ -10,7 +10,6 @@ case class ListItem(marker: String, text: Text, content: Option[Sast])
 
 sealed trait Inline
 case class InlineText(str: String, quoted: Int = 0) extends Inline:
-  require(str.nonEmpty)
   override def toString: String = s"“$str”"
 
 case class Directive(command: DCommand, attributes: Attributes)(val prov: Prov) extends Inline
